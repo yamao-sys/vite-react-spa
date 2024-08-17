556ebe15cd6a1ee7a1889153d9f97509eaad9b9f
yamao-sys <yamaopanku@gmail.com>
2024-08-17 19:06:34 +0900

    ルーティングを定数化

---
+++ frontend/src/src/constants/navigation/index.ts
@@ -0,0 +1,15 @@
+export const NAVIGATION_LIST = {
+  top: '/',
+  auth: {
+    signUp: '/auth/sign_up',
+    signIn: '/auth/sign_in',
+  },
+};
+
+export const NAVIGATION_PAGE = {
+  top: '/',
+  auth: {
+    signUp: '/auth/sign_up',
+    signIn: '/auth/sign_in',
+  },
+};
--- frontend/src/src/routes/index.tsx
+++ frontend/src/src/routes/index.tsx
@@ -1,16 +1,22 @@
 import App from '@/App';
 import { NotFound } from '@/components/organisms/NotFound';
+import { NAVIGATION_LIST } from '@/constants/navigation';
+import { SignInPage } from '@/pages/auth/sign_in';
 import { SignUpPage } from '@/pages/auth/sign_up';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: NAVIGATION_LIST.top,
     element: <App />,
   },
   {
-    path: '/auth/sign_up',
+    path: NAVIGATION_LIST.auth.signUp,
     element: <SignUpPage />,
+  },
+  {
+    path: NAVIGATION_LIST.auth.signIn,
+    element: <SignInPage />,
   },
   {
     path: '/*',
