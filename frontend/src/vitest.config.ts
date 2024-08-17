4f6931fe5df84f2ea4a075b4f37c18a2b0528d4e
yamao-sys <yamaopanku@gmail.com>
2024-08-17 09:57:45 +0900

    vitestの追加

---
+++ frontend/src/vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+import * as path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.test.{js,ts,jsx,tsx}'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
