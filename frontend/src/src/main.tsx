9fe4c206572c9ee2fd7ca9f0458c8188d73cc560
yamao-sys <yamaopanku@gmail.com>
2024-08-18 11:56:50 +0900

    コンテキストの範囲をRouter配下にする

--- frontend/src/src/main.tsx
+++ frontend/src/src/main.tsx
@@ -1,11 +1,35 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/index.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Layout } from './layout.tsx';
+import App from './App.tsx';
+import { NAVIGATION_LIST } from './constants/navigation/index.ts';
+import { SignUpPage } from './pages/auth/sign_up/index.tsx';
+import { SignInPage } from './pages/auth/sign_in/index.tsx';
+import { NotFound } from './components/organisms/NotFound/index.tsx';
+import { ReadingRecordListsPage } from './pages/reading_records/lists/index.tsx';
+import { ReadingRecordCreatePage } from './pages/reading_records/new/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path={NAVIGATION_LIST.top} element={<App />}></Route>
+          <Route path={NAVIGATION_LIST.auth.signUp} element={<SignUpPage />}></Route>
+          <Route path={NAVIGATION_LIST.auth.signIn} element={<SignInPage />}></Route>
+          <Route
+            path={NAVIGATION_LIST.readingRecords.list}
+            element={<ReadingRecordListsPage />}
+          ></Route>
+          <Route
+            path={NAVIGATION_LIST.readingRecords.new}
+            element={<ReadingRecordCreatePage />}
+          ></Route>
+          <Route path='/*' element={<NotFound />}></Route>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   </StrictMode>,
 );
