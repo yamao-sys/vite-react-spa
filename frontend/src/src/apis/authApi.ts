6debe2555b3e5a7212a6e045593e28c4e0ebf614
yamao-sys <yamaopanku@gmail.com>
2024-08-18 10:22:15 +0900

    APIとの通信を行うメソッド群をapisディレクトリにまとめる

---
+++ frontend/src/src/apis/authApi.ts
@@ -0,0 +1,42 @@
+import api from '@/generated/auth/$api';
+import { SignUpDto } from '@/generated/auth/@types';
+import aspida, { FetchConfig } from '@aspida/fetch';
+
+const getAuthApiClient = (options?: FetchConfig) => {
+  const baseFetchConditions = {
+    baseURL: import.meta.env.VITE_BASE_API_URL,
+    throwHttpErrors: true,
+  };
+
+  return api(aspida(fetch, { ...baseFetchConditions, ...(options || {}) }));
+};
+
+export const postValidateSignUp = async (data: SignUpDto) => {
+  const response = await getAuthApiClient().auth.validateSignUp.post({
+    body: {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      passwordConfirm: data.passwordConfirm,
+    },
+  });
+
+  return { errors: response.body.errors };
+};
+
+export const postSignUp = async (data: SignUpDto) => {
+  const response = await getAuthApiClient().auth.signUp.post({
+    body: {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      passwordConfirm: data.passwordConfirm,
+    },
+  });
+
+  return { result: response.body.result };
+};
+
+export const checkSignedIn = async () => {
+  return await getAuthApiClient({ credentials: 'include' }).auth.checkSignedIn.$get();
+};
